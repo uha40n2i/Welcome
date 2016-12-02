2ceec6d37ca340f42ccbb9420b7ae4b25bb6d4e0
Alvin FREY <alvin.frey@yahoo.fr>
2016-12-02 04:32:00 +0100

    Contacts le lol

--- application/view/contacts.php
+++ application/view/contacts.php
@@ -1,3 +1,31 @@
+
+<link href="<?php echo URL; ?>css/contacts.css" rel="stylesheet">
+
 <div class="container">
-    <p>Salut</p>
+    <br/>
+
+    <center>
+
+        <button class="button-primary" id="host-button">Hébergements</button>
+
+        <br/>
+        <br/>
+
+        <input class="u-full-width" type="text" placeholder="Votre emplacement (rue + ville)" id="location-input">
+
+        <hr>
+
+    </center>
+
+        <br/>
+
+        <div class="results-contacts">
+
+        </div>
+
+    </div>
+
 </div>
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="  crossorigin="anonymous"></script>
+<script src="<?php echo URL; ?>js/contacts.js"></script>
---
+++ public/js/contacts.js
@@ -0,0 +1,64 @@
+
+var radiusKm = 20;
+
+function distance(lat1, lon1, lat2, lon2) {
+    var p = 0.017453292519943295;    // Math.PI / 180
+    var c = Math.cos;
+    var a = 0.5 - c((lat2 - lat1) * p)/2 +
+        c(lat1 * p) * c(lat2 * p) *
+        (1 - c((lon2 - lon1) * p))/2;
+
+    return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+}
+
+$("#host-button").click(function(){
+
+    $.ajax({
+
+        url: "http://welcomen2i.eu/data/hebergements.json"
+
+    }).done(function( dataHebergements ) {
+
+        var location = $("#location-input").val();
+
+        if(location==""){
+
+            for(var hebergements in dataHebergements.markers.marker){
+
+                $(".results-contacts").append('<div class="cadre-contacts"><h6><a href="'+ dataHebergements.markers.marker[hebergements]._url +'" target="_blank">'+ dataHebergements.markers.marker[hebergements]._nom +'</a> à '+dataHebergements.markers.marker[hebergements]._ville +'</h6></div> <br/>');
+
+            }
+
+        }else{
+
+            $.ajax({
+
+                url: "http://api.opencagedata.com/geocode/v1/json?q="+ encodeURI($("#location-input").val()) +"&key=ec7d80425487db49c7d5a28f2b96f3e3"
+
+            }).done(function( dataGeocoding ) {
+
+                var latitude = dataGeocoding.results[0].geometry.lat;
+                var longitude = dataGeocoding.results[0].geometry.lng;
+                var postCode = dataGeocoding.results[0].components.postcode;
+                var city = dataGeocoding.results[0].components.town;
+                var qibla = dataGeocoding.results[0].annotations.qibla;
+                var mapUrl = dataGeocoding.results[0].annotations.OSM.url;
+
+                $(".results-contacts").empty();
+
+                for(var hebergements in dataHebergements.markers.marker){
+
+                    if (distance(latitude, longitude, dataHebergements.markers.marker[hebergements]._lat, dataHebergements.markers.marker[hebergements]._lng) <= radiusKm){
+
+                        $(".results-contacts").append('<div class="cadre-contacts"><h6><a href="'+ dataHebergements.markers.marker[hebergements]._url +'" target="_blank">'+ dataHebergements.markers.marker[hebergements]._nom +'</a> à '+dataHebergements.markers.marker[hebergements]._ville +'</h6><p>L\'emplacement se trouve à <b>'+ Math.floor(distance(latitude, longitude, dataHebergements.markers.marker[hebergements]._lat, dataHebergements.markers.marker[hebergements]._lng)) +'km</b><p> Cliquez <a href="https://www.google.fr/maps/dir/\''+ latitude +','+ longitude +'\'/'+ dataHebergements.markers.marker[hebergements]._lat +','+ dataHebergements.markers.marker[hebergements]._lng +'/" target="_blank">ici</a> pour voir l\'itinéraire vous menant jusqu\'à l\'emplacement</p></div> <br/>');
+
+                    }
+
+                }
+
+            });
+        }
+
+    });
+
+});
