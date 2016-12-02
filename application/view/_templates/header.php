58660ab19fe701a84711516a5c9fa728d89cf7ff
Alvin FREY <alvin.frey@yahoo.fr>
2016-12-02 08:15:20 +0100

    okkelkechose

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -9,6 +9,19 @@
     <link href="<?php echo URL; ?>css/skeleton.css" rel="stylesheet">
     <link href="<?php echo URL; ?>css/main.css" rel="stylesheet">
     <link href="<?php echo URL; ?>css/alerts.css" rel="stylesheet">
+    <link href="<?php echo URL; ?>css/header.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="../../public/css/header.css"
+
 </head>
 <body>
+
+<div class="conteneur-header">
+    <a class="header-menu" href="/"><h1 class="header-title">WELCOME</h1></a>
+    <ul class="menu-navbar">
+        <li class="liste-navbar"><a class="bouton-menu" href="/pages/contacts/">Contact</a></li>
+        <li class="liste-navbar"><a class="bouton-menu" href="/pages/emergency/">Urgence</a></li>
+        <li class="liste-navbar"><a class="bouton-menu" href="/adverts/">Annonces</a></li>
+    </ul>
+</div>
 
